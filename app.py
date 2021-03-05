ea4aa40552024abee23adb1f6889cb1fc2f5b9ad
aryamakwana <arya18mak@gmail.com>
2021-03-05 16:46:03 +0530

    initial commit

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask import Flask, request, jsonify, render_template
 from sklearn.ensemble import AdaBoostRegressor
 import pickle
 import sqlite3
-
+import psycopg2
 app = Flask(__name__)
 reg1 = pickle.load(open('x_cord', 'rb'))
 reg2 = pickle.load(open('y_cord', 'rb'))
-
+DB_HOST = "ec2-3-223-72-172.compute-1.amazonaws.com"
+DB_NAME = "d5habih2mgsfqu"
+DB_USER = "ojwlqolbgopaus"
+DB_PASS = "70d25ffa67f05b1532833e77fa53198f92bde5c67bdf9cbf6fb5815c2faa7487"
 
 @app.route('/')
 def home():
@@ -16,25 +19,12 @@ def home():
 
 @app.route('/database')
 def database():
-    conn = sqlite3.connect('values.db')
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
     cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS coord_table (
-        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-        coord_1 float,
-        coord_2 float
-        )""")
-    cur.execute("INSERT INTO coord_table VALUES(:Id, :coord_1, :coord_2)",
-                {'Id': None,
-                 'coord_1': float(4),
-                 'coord_2': float(2)
-                 }
-                )
-    entry = list(cur.execute("SELECT * FROM coord_table"))
-    conn.commit()
-
-    cur.close()
-    conn.close()
-    return jsonify(entry)
+    cur.execute("CREATE TABLE student (id SERIAL PRIMARY KEY, name VARCHAR);")
+    cur.execute("INSERT INTO student (name) VALUES(%s)", ("Cristina",))
+    cur.execute("SELECT * FROM student;")
+    print(cur.fetchall())
 
 @app.route('/predict', methods=['GET'])
 def predict():
