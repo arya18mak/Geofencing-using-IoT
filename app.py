f8a2da6246fae4dee377f4d1d042d463f7976e44
aryamakwana <arya18mak@gmail.com>
2021-03-05 17:54:38 +0530

    initial commit

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def database():
     cur.execute("SELECT * FROM student;")
     row1 = cur.fetchall()
     conn.commit()
+    conn.close()
     return "{}".format(row1)
 
 @app.route('/predict', methods=['GET'])
